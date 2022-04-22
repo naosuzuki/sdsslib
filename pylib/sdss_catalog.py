78284e4ed222370595582d8e987a0b3cec9f972f
Nao Suzuki <nao.suzuki@gmail.com>
2022-04-21 19:54:36 -0700

    NS updates

--- pylib/sdss_catalog.py
+++ pylib/sdss_catalog.py
@@ -57,9 +57,9 @@ class SDSSspec:
           #self.flux=fits[1][self.fiber-1,:]
           #self.ivar=self.fits[2][self.fiber-1,:]
           #self.mask=self.fits[3][self.fiber-1,:]
-          self.flux=fits[1][551:551,:]
-          self.ivar=fits[2][551:551,:]
-          self.mask=fits[3][551:551,:]
+          self.flux=self.fits[1][551:551,:]
+          self.ivar=self.fits[2][551:551,:]
+          self.mask=self.fits[3][551:551,:]
           self.wave=10.0**(self.coeff0+self.coeff1*numpy.arange(self.npix))
           self.err=self.ivar
           self.wid=int(h['coeff0']/0.0001)+numpy.arange(self.npix)
