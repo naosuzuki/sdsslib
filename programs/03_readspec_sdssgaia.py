1f3649b7a6f3499526d7fac45b0cd656126d54ca
Nao Suzuki <nao.suzuki@gmail.com>
2022-07-24 17:09:54 -0700

    revise 03

--- programs/03_readspec_sdssgaia.py
+++ programs/03_readspec_sdssgaia.py
@@ -28,17 +28,6 @@ def read_spec(csvfile,fitsfilename):
    dftmp=df[(df['plate']==plate) & (df['mjd']==mjd)]
    dftmp.reset_index()
 
-   matplotlib.use('Agg')
-   # Figure Resolution
-   plt.rcParams["figure.dpi"] = 144
-   # Font to be Times
-   plt.rc('font',family='serif')
-   plt.xlabel('Wavelength',fontsize=15)
-   plt.ylabel('Flux (10$^{-17}$ erg/Ang/s/cm$^{2}$)',fontsize=15)
-   plt.tick_params(axis='both',direction='in')
-   xmin=3300.;  xmax=10500.;  ymin=0.1
-   plt.xlim([xmin,xmax])
-   dx=xmax-xmin
 
    print(len(dftmp))
    for i in range(len(dftmp)):
@@ -52,30 +41,42 @@ def read_spec(csvfile,fitsfilename):
       gaiaxp.read(source_id)
       # GAIA data in 20 points
       gaiaxp.photopoints20()
-      #for j in range(20):
-        #print(gaiaxp.wave[j],gaiaxp.flux[j],gaiaxp.wid[j])
-      #  print(gaiaxp.ptx[j],gaiaxp.pty[j],gaiaxp.ptwid[j])
-      #sys.exit(1)
 
       print(plate,mjd,fiber,source_id)
       # Reading SDSS data
       spec=sdss_db.SDSSspec(plate,mjd,fiber)
       spec.read()
       spec.photopoints20()
-      #sys.exit(1)
+      # Plot
+      gaia_sdss(gaiaxp,spec)
 
-      specname=spec.strplate+'+'+spec.strmjd+'+'+spec.strfiber
-# Plot
-      ymax=max(spec.flux)
-      plt.ylim([ymin,ymax])
-      dy=ymax-ymin
-      plt.plot(spec.wave,spec.flux,linewidth=1.0,color='b',alpha=0.5)
-      plt.plot(gaiaxp.wave,gaiaxp.flux,linewidth=2.0,color='r',alpha=0.7)
-      plt.errorbar(gaiaxp.ptx,gaiaxp.pty,yerr=gaiaxp.ptyerr,fmt='o',color='magenta',elinewidth=2,capsize=3)
-      plt.errorbar(spec.ptx,spec.pty,yerr=spec.ptyerr,fmt='o',color='cyan',elinewidth=2,capsize=3)
-      plt.text(xmin+dx*0.48,ymin+0.90*dy,specname,fontsize=18)
-      plt.savefig(specname+'.png')
-      plt.clf()
+
+def gaia_sdss(gaiaxp,spec):
+   specname=spec.strplate+'+'+spec.strmjd+'+'+spec.strfiber
+
+   matplotlib.use('Agg')
+   # Figure Resolution
+   plt.rcParams["figure.dpi"] = 144
+   # Font to be Times
+   plt.rc('font',family='serif')
+   plt.xlabel('Wavelength',fontsize=15)
+   plt.ylabel('Flux (10$^{-17}$ erg/Ang/s/cm$^{2}$)',fontsize=15)
+   plt.tick_params(axis='both',direction='in')
+
+   xmin=3300.;  xmax=10500.;  ymin=0.1
+   plt.xlim([xmin,xmax])
+   dx=xmax-xmin
+   ymax=max(spec.flux)
+   plt.ylim([ymin,ymax])
+   dy=ymax-ymin
+
+   plt.plot(spec.wave,spec.flux,linewidth=1.0,color='b',alpha=0.5)
+   plt.plot(gaiaxp.wave,gaiaxp.flux,linewidth=2.0,color='r',alpha=0.7)
+   plt.errorbar(gaiaxp.ptx,gaiaxp.pty,yerr=gaiaxp.ptyerr,fmt='o',color='magenta',elinewidth=2,capsize=3)
+   plt.errorbar(spec.ptx,spec.pty,yerr=spec.ptyerr,fmt='o',color='cyan',elinewidth=2,capsize=3)
+   plt.text(xmin+dx*0.48,ymin+0.90*dy,specname,fontsize=18)
+   plt.savefig(specname+'.png')
+   plt.clf()
    plt.close()
 
 def read_2dspec(fitsfilename):
