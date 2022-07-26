b29de65cb494b10febe0685b20294d79d37916f6
Nao Suzuki <nao.suzuki@gmail.com>
2022-07-26 14:51:26 -0700

    revision

--- programs/03_readspec_sdssgaia.py
+++ programs/03_readspec_sdssgaia.py
@@ -2,6 +2,8 @@ import fitsio
 import sys
 import os
 import numpy
+import scipy
+from scipy import optimize
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import colors
@@ -27,15 +29,18 @@ def read_spec(csvfile,fitsfilename):
    plate=6783 ;  mjd=56284
    dftmp=df[(df['plate']==plate) & (df['mjd']==mjd)]
    dftmp.reset_index()
-
+   print(dftmp)
+   sys.exit(1)
 
    print(len(dftmp))
    for i in range(len(dftmp)):
    #for i in range(4,5):
-      mjd=dftmp['mjd'].iloc[i]
-      fiber=dftmp['fiber'].iloc[i]
+      mjd      =dftmp['mjd'].iloc[i]
+      fiber    =dftmp['fiber'].iloc[i]
       source_id=dftmp['source_id'].iloc[i]
-      print('source_id=',source_id)
+      #print('source_id=',source_id)
+      lambdaeff=dftmp['lambdaeff'].iloc[i]
+      zoffset  =dftmp['lambdaeff'].iloc[i]
    
       # Reading GAIA data
       gaiaxp.read(source_id)
@@ -47,8 +52,134 @@ def read_spec(csvfile,fitsfilename):
       spec=sdss_db.SDSSspec(plate,mjd,fiber)
       spec.read()
       spec.photopoints20()
+      print('lambdaeff',spec.lambdaeff)
+      sys.exit(1)
+    
       # Plot
-      gaia_sdss(gaiaxp,spec)
+      #gaia_sdss(gaiaxp,spec)
+      [ratio_ptx,ratio_pty,ratio_err]=sdss_gaira_ratios(gaiaxp,spec)
+      #for j in range(len(ratio_ptx)):
+      #   print(ratio_ptx[j],ratio_pty[j])
+# Plot
+      gaia_sdss_panels(gaiaxp,spec,ratio_ptx,ratio_pty,ratio_err)
+#     [curve_x,curve_y]=fitting_curve(ratio_ptx,ratio_pty,ratio_err)
+#     sys.exit(1)
+
+      #sys.exit(1)
+
+def sdss_gaira_ratios(gaiaxp,spec):
+   flag_gaia=numpy.where(gaiaxp.ptmask==1,1,0)
+   flag_sdss=numpy.where(gaiaxp.ptmask==1,1,0)
+   print('flag_gaia',flag_gaia)
+   print('flag_sdss',flag_sdss)
+   flag=flag_gaia*flag_sdss
+   print('total flag',flag)
+
+   ratio_ptx=numpy.compress(flag,gaiaxp.ptx)
+
+   gaia_pty=numpy.compress(flag,gaiaxp.pty)
+   gaia_err=numpy.compress(flag,gaiaxp.ptyerr)
+   gaia_snr=gaia_pty/gaia_err
+   sdss_pty=numpy.compress(flag,spec.pty)
+   sdss_err=numpy.compress(flag,spec.ptyerr)
+   sdss_snr=sdss_pty/sdss_err
+   ratio_pty=sdss_pty/gaia_pty
+
+   ratio_snr=numpy.zeros(len(sdss_snr))
+   ratio_err=numpy.zeros(len(sdss_snr))
+   for i in range(len(sdss_snr)):
+      if(sdss_snr[i]<gaia_snr[i]):
+         ratio_snr[i]=sdss_snr[i]
+      else:
+         ratio_snr[i]=gaia_snr[i]
+      ratio_err[i]=ratio_pty[i]/ratio_snr[i]
+   return [ratio_ptx,ratio_pty,ratio_err]
+ 
+   #print('selected xpt=',gaia_ptx)
+   #ratio_snr=min(gaia_snr,sdss_snr)
+   #print('snr=',ratio_snr)
+
+def fitting_curve(ratio_ptx,ratio_pty,ratio_err):
+
+   fitfunc = lambda p, x: p[0]*((x/10000.0)**p[1])*numpy.exp(p[2]*(x/10000.0))+p[3]
+   #errfunc = lambda p, x, y: (fitfunc(p,x) - y)**2
+   errfunc = lambda p, x, y, z: (fitfunc(p,x) - y)**2/z**2
+   p0 = [1.0,0.1,-1.0,0.1]
+   #p0 = [1.0,0.1,-1.0]
+   #p1,success=scipy.optimize.leastsq(errfunc,p0[:],args=(rwave,rflux),maxfev=10000)
+   #p1,success=scipy.optimize.leastsq(errfunc,p0[:],args=(xtmp,ytmp),maxfev=10000)
+   #p1,success=scipy.optimize.leastsq(errfunc,p0[:],args=(ratio_ptx,ratio_pty),maxfev=10000)
+   p1,success=scipy.optimize.leastsq(errfunc,p0[:],args=(ratio_ptx,ratio_pty,ratio_err),maxfev=10000)
+   curve_x=3000.0+numpy.arange(750)*10 ; curve_y=numpy.zeros(100)
+   curve_y=p1[0]*((curve_x/10000.0)**p1[1])*numpy.exp(p1[2]*(curve_x/10000.0))+p1[3]
+   
+   print(p1)
+   return [curve_x,curve_y]
+
+def fitting_line(ratio_ptx,ratio_pty,ratio_err):
+
+   fitfunc = lambda p, x: p[0]*(x/10000.0)+p[1]
+   errfunc = lambda p, x, y, z: (fitfunc(p,x) - y)**2/z**2
+   #p0 = [1.0,0.1,-1.0,0.1]
+   p0 = [0.0,1.0]
+   p1,success=scipy.optimize.leastsq(errfunc,p0[:],args=(ratio_ptx,ratio_pty,ratio_err),maxfev=10000)
+
+   line_x=3000.0+numpy.arange(750)*10 ; line_y=numpy.zeros(100)
+   line_y=p1[0]*(line_x/10000.0)+p1[1]
+   #curve_y=p1[0]*((curve_x/10000.0)**p1[1])*numpy.exp(p1[2]*(curve_x/10000.0))+p1[3]
+  
+   #print(p1)
+   return [line_x,line_y]
+
+def gaia_sdss_panels(gaiaxp,spec,ratio_ptx,ratio_pty,ratio_err):
+
+# Fit Curve
+   [curve_x,curve_y]=fitting_curve(ratio_ptx,ratio_pty,ratio_err)
+   [line_x,line_y]  =fitting_line(ratio_ptx,ratio_pty,ratio_err)
+
+   specname=spec.strplate+'+'+spec.strmjd+'+'+spec.strfiber
+
+   # Limits
+   xmin=3300.;  xmax=10500.
+   dx=xmax-xmin
+   ymin=0.1  ; ymax=max(spec.flux)*1.15 
+   dy=ymax-ymin
+
+   matplotlib.use('Agg')
+   # Figure Resolution
+   plt.rcParams["figure.dpi"] = 144
+   # Font to be Times
+   plt.rc('font',family='serif')
+
+   fig,axs=plt.subplots(2,1,sharex=True,gridspec_kw={'height_ratios': [3, 1]})
+
+   # Bottom Panel
+   zero_ptx=numpy.array([xmin,xmax]) ; zero_pty=numpy.array([1.0,1.0])
+   axs[1].set_xlabel('Wavelength',fontsize=12)
+   axs[1].set_ylabel('Ratio',fontsize=12)
+   axs[1].set_xlim([xmin,xmax])
+   axs[1].set_ylim([0.6,1.4])
+   axs[1].plot(zero_ptx,zero_pty,'--')
+   axs[1].plot(curve_x,curve_y,'-',color='salmon')
+   axs[1].plot(line_x,line_y,':',color='blue')
+   axs[1].errorbar(ratio_ptx,ratio_pty,yerr=ratio_err,fmt='o',color='red',elinewidth=2,capsize=3)
+
+   axs[0].set_ylabel('Flux (10$^{-17}$ erg/Ang/s/cm$^{2}$)',fontsize=12)
+   axs[0].set_xlim([xmin,xmax])
+   axs[0].set_ylim([ymin,ymax])
+   axs[0].tick_params(axis='both',direction='in')
+   # SDSS vs GAIA spec
+   axs[0].plot(spec.wave,spec.flux,linewidth=1.0,color='b',alpha=0.5)
+   axs[0].plot(gaiaxp.wave,gaiaxp.flux,linewidth=2.0,color='r',alpha=0.7)
+   # SDSS vs GAIA photometry points
+   axs[0].errorbar(gaiaxp.ptx,gaiaxp.pty,yerr=gaiaxp.ptyerr,fmt='o',color='magenta',elinewidth=2,capsize=3)
+   axs[0].errorbar(spec.ptx,spec.pty,yerr=spec.ptyerr,fmt='o',color='cyan',elinewidth=2,capsize=3)
+   # Spectrum Name
+   axs[0].text(xmin+dx*0.48,ymin+0.90*dy,specname,fontsize=18)
+
+   plt.savefig(specname+'.png')
+   plt.clf()
+   plt.close()
 
 
 def gaia_sdss(gaiaxp,spec):
