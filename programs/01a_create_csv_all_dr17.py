950c36162c4706f91dfdd0cdcad38ce29ce63717
Nao Suzuki <nao.suzuki@gmail.com>
2022-08-06 14:24:50 -0700

    01a updates

--- programs/01a_create_csv_all_dr17.py
+++ programs/01a_create_csv_all_dr17.py
@@ -131,7 +131,28 @@ if(objtype=='galaxy'):
 # Quasar
 objtype='quasar'
 if(objtype=='quasar'):
-   products_list=[spall.platelist,spall.mjdlist,spall.fiberlist,\
+#   products_list=[spall.platelist,spall.mjdlist,spall.fiberlist,\
+#               spall.ralist,spall.declist,\
+#               spall.thing_idlist,\
+#               spall.classlist,spall.subclasslist,\
+#               spall.psfmaglist[:,0],spall.psfmagerrlist[:,0],\
+#               spall.psfmaglist[:,1],spall.psfmagerrlist[:,1],\
+#               spall.psfmaglist[:,2],spall.psfmagerrlist[:,2],\
+#               spall.psfmaglist[:,3],spall.psfmagerrlist[:,3],\
+#               spall.psfmaglist[:,4],spall.psfmagerrlist[:,4],\
+#               spall.lambdaefflist,\
+#               spall.xfocallist,spall.yfocallist,spall.zoffsetlist,\
+#               spall.sn_medianalllist,spall.zspzbestlist,spall.zspzbesterrlist,spall.zwarninglist]
+#   df=pd.DataFrame(products_list).transpose()
+#   df.columns=['plate','mjd','fiber','ra','dec','thing_id','class','subclass',\
+#               'psfmag_u','psfmagerr_u',\
+#               'psfmag_g','psfmagerr_g',\
+#               'psfmag_r','psfmagerr_r',\
+#               'psfmag_i','psfmagerr_i',\
+#               'psfmag_z','psfmagerr_z',\
+#               'lambdaeff','xfocal','yfocal','zoffset',\
+#               'snall','z','zerr','zwarning']
+   df=pd.DataFrame(list(zip(spall.platelist,spall.mjdlist,spall.fiberlist,\
                spall.ralist,spall.declist,\
                spall.thing_idlist,\
                spall.classlist,spall.subclasslist,\
@@ -140,18 +161,30 @@ if(objtype=='quasar'):
                spall.psfmaglist[:,2],spall.psfmagerrlist[:,2],\
                spall.psfmaglist[:,3],spall.psfmagerrlist[:,3],\
                spall.psfmaglist[:,4],spall.psfmagerrlist[:,4],\
+               spall.spectromaglist[:,0],spall.spectromagerrlist[:,0],\
+               spall.spectromaglist[:,1],spall.spectromagerrlist[:,1],\
+               spall.spectromaglist[:,2],spall.spectromagerrlist[:,2],\
+               spall.spectromaglist[:,3],spall.spectromagerrlist[:,3],\
+               spall.spectromaglist[:,4],spall.spectromagerrlist[:,4],\
                spall.lambdaefflist,\
                spall.xfocallist,spall.yfocallist,spall.zoffsetlist,\
-               spall.sn_medianalllist,spall.zspzbestlist,spall.zspzbesterrlist,spall.zwarninglist]
-   df=pd.DataFrame(products_list).transpose()
-   df.columns=['plate','mjd','fiber','ra','dec','thing_id','class','subclass',\
+               spall.sn_medianalllist,\
+               spall.elodie_objectlist,\
+               spall.elodie_sptypelist,spall.elodie_bvlist,spall.elodie_fehlist,\
+               spall.elodie_tefflist,spall.elodie_logglist)),\
+               columns=['plate','mjd','fiber','ra','dec','thing_id','class','subclass',\
                'psfmag_u','psfmagerr_u',\
                'psfmag_g','psfmagerr_g',\
                'psfmag_r','psfmagerr_r',\
                'psfmag_i','psfmagerr_i',\
                'psfmag_z','psfmagerr_z',\
+               'specmag_u','specmagerr_u',\
+               'specmag_g','specmagerr_g',\
+               'specmag_r','specmagerr_r',\
+               'specmag_i','specmagerr_i',\
+               'specmag_z','specmagerr_z',\
                'lambdaeff','xfocal','yfocal','zoffset',\
-               'snall','z','zerr','zwarning']
+               'snall','object','sptype','bv','feh','teff','logg'])
    print(df)
    dfquasar=df[(df['class']=='QSO   ') & (df['thing_id']!=-1)]
    dfquasar.to_csv('../csvfiles/v5_13_2_spall_quasar.csv',index=False)
