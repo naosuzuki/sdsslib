2847064eb598af4e18325d7b3d9438d863895b4e
Nao Suzuki <nao.suzuki@gmail.com>
2022-08-14 12:33:46 -0700

    fix flag gaia

--- pylib/sdss_db.py
+++ pylib/sdss_db.py
@@ -303,7 +303,7 @@ def create_2dspec(df,fitsfilename,objtype,flag_gaia):
       col20=fits.Column(name='logg',format='E',array=logg_list)
       cols=fits.ColDefs([col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,\
                         col11,col12,col13,col14,col15,col16,col17,col18,col19,col20])
-      if(gaia_flag):
+      if(flag_gaia):
          col21=fits.Column(name='parallax',format='E',array=parallax_list)
          col22=fits.Column(name='parallaxerr',format='E',array=parallaxerr_list)
          col23=fits.Column(name='parallaxsnr',format='E',array=parallaxsnr_list)
